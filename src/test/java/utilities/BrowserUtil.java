c98c0f7f3363582479b5f17c03a9378cfac666dd
Alparslan <alparslanztk@gmail.com>
2024-02-25 01:18:33 +0300

    New codes added about actions

---
+++ src/test/java/utilities/BrowserUtil.java
@@ -0,0 +1,19 @@
+package utilities;
+
+public class BrowserUtil {
+
+    /**
+     * Performs a pause
+     * @param seconds
+     */
+    public static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
