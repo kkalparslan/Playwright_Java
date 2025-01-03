7fd43cbd8ab80af265faa3c8dd476f1d239d0cfb
Alparslan <alparslanztk@gmail.com>
2025-01-03 09:25:33 +0300

    some arrangments done.

--- src/test/java/d1_openBrowser/OpenBrowser.java
+++ src/test/java/d1_openBrowser/OpenBrowser.java
@@ -25,7 +25,7 @@ public class OpenBrowser {
                  * kapanması için aşağıdaki gibi ayrı ayrı close() methodları kullandım.
                  */
                 Playwright playwright = Playwright.create();
-                Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+                Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
                 Page page = browser.newPage();
                 page.navigate("http://playwright.dev");
                 System.out.println(page.title());
